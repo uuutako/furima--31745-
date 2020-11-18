b3cf8ea6c3830d48897899e80f84d1ff7ef2707f
uuutako <okn.sk009@gmail.com>
2020-11-18 22:50:49 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit] 
+  #ログインしてない人はログイン画面に飛ばされる
+
+  before_action :move_to_index, only: [:edit]
+  #アイテムのユーザーIDとログインしているユーザーが異なる場合トップページに戻る
+  #特定のアクションのときだけbefore_actionを使いたい場合はonly
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -21,6 +26,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 end
 
   private
@@ -28,3 +47,11 @@ end
 def item_params
   params.require(:item).permit(:name, :concept, :category_id, :condition_id, :shipping_id, :region_id, :postage_id, :image, :price).merge(user_id: current_user.id)
 end
+
+def move_to_index
+  @item = Item.find(params[:id])
+  unless @item.user == current_user
+  redirect_to action: :index
+  end
+end
+
