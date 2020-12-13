ad9bc8376f2312fbadeb3e322ca5c9f65dd7d56a
uuutako <okn.sk009@gmail.com>
2020-12-13 11:53:02 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'Capistranoのバージョン'
-lock "~> 3.14.1"
+lock '3.14.1'
+
 # Capistranoのログの表示に利用する
 set :application, "furima--31745-"
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "uuutako/furima--31745-"
+set :repo_url, "git@github.com:uuutako/furima--31745-.git"
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
